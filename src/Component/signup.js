4b0b85b36ae98814eb6f2a7ba4d053890130ba6a
mariyasada <mariyasada2406@gmail.com>
2022-10-07 17:06:56 +0530

    adding

--- src/Component/signup.js
+++ src/Component/signup.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const signup = () => {
+const Signup = () => {
   return (
     <div>
       <h3>Signup branch</h3>
@@ -8,4 +8,4 @@ const signup = () => {
   )
 }
 
-export default signup
+export default Signup
