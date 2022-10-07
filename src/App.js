370020b72e347274cb35f9f01978729c016127af
mariyasada <mariyasada2406@gmail.com>
2022-10-07 16:41:35 +0530

    feat:added signup component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hedear from './Component/Hedear';
 import Login from './Component/login';
+import Signup from './Component/signup';
 
 function App() {
   return (
     <div className="App">
       <Hedear/>
       <Login/>
+      <Signup/>
     </div>
   );
 }
---
+++ src/Component/signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>
+      <h3>Signup branch</h3>
+    </div>
+  )
+}
+
+export default signup
